7570356c9bd27727e1c1ce41f24ecd2ce35ab2ee
d8ahazard <d8ahazard@gmail.com>
2019-12-18 16:09:37 -0600

    Apparently, docker doesn't like starting services this way...

--- Program.cs
+++ Program.cs
@@ -1,7 +1,5 @@
-using HueDream.Models.DreamScreen;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 
 namespace HueDream {
     public static class Program {
@@ -9,11 +7,8 @@ namespace HueDream {
             CreateHostBuilder(args).Build().Run();
         }
 
-        private static IHostBuilder CreateHostBuilder(string[] args) {
-            return Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); })
-                // Initialize our dream screen emulator
-                .ConfigureServices(services => { services.AddHostedService<DreamClient>(); });
+        private static IWebHostBuilder CreateHostBuilder(string[] args) {
+            return WebHost.CreateDefaultBuilder(args).UseStartup<Startup>();
         }
     }
 }
--- Startup.cs
+++ Startup.cs
@@ -1,7 +1,6 @@
 using System;
-using System.Net.WebSockets;
 using System.Threading;
-using System.Threading.Tasks;
+using HueDream.Models.DreamScreen;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -10,13 +9,23 @@ using Microsoft.Extensions.Hosting;
 
 namespace HueDream {
     public class Startup : IDisposable {
+        public DreamClient Dc;
+        private CancellationTokenSource ct;
+
         public Startup(IConfiguration configuration) {
+            Console.WriteLine(@"Startup called...");
             Configuration = configuration;
+            Dc = new DreamClient();
+            ct = new CancellationTokenSource();
+            Dc.StartAsync(ct.Token);
         }
 
         public IConfiguration Configuration { get; }
 
-        public void Dispose() { }
+        public void Dispose() {
+            ct.Cancel();
+            Dc.Dispose();
+        }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public static void ConfigureServices(IServiceCollection services) {
@@ -37,20 +46,7 @@ namespace HueDream {
             app.UseRouting();
 
             app.UseAuthorization();
-            // SOCKET-TO-ME
-            /*app.UseWebSockets();
-            app.Map("/ws", builder => {
-                builder.Use(async (context, next) => {
-                    if (context.WebSockets.IsWebSocketRequest) {
-                        var webSocket = await context.WebSockets.AcceptWebSocketAsync().ConfigureAwait(true);
-                        await Echo(webSocket).ConfigureAwait(true);
-                        return;
-                    }
-                    await next();
-                });
-            });
-*/
-
+            
             app.UseEndpoints(endpoints => {
                 endpoints.MapControllerRoute(
                     "default",
@@ -58,19 +54,5 @@ namespace HueDream {
             });
         }
 
-        private async Task Echo(WebSocket webSocket) {
-            var buffer = new byte[1024 * 4];
-            var result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None)
-                .ConfigureAwait(true);
-            while (!result.CloseStatus.HasValue) {
-                await webSocket.SendAsync(new ArraySegment<byte>(buffer, 0, result.Count), result.MessageType,
-                    result.EndOfMessage, CancellationToken.None).ConfigureAwait(true);
-                result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None)
-                    .ConfigureAwait(true);
-            }
-
-            await webSocket.CloseAsync(result.CloseStatus.Value, result.CloseStatusDescription, CancellationToken.None)
-                .ConfigureAwait(true);
-        }
     }
 }
