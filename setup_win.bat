32440957085e4a52983cc1bbee8db63728ced4c8
d8ahazard <d8ahazard@gmail.com>
2020-05-26 10:46:21 -0500

    Update windows setup script

--- setup_win.bat
+++ setup_win.bat
@@ -1,10 +1,9 @@
 @echo off
-echo "Downloading git exe"
 set installDir="C:\Program Files\Git"
 set installDir=%installDir:"=%
 git
 if errorlevel 0 goto installGlimmr
-
+echo "Downloading git exe"
 set url=https://github.com/git-for-windows/git/releases/download/v2.26.2.windows.1/Git-2.26.2-64-bit.exe
 set file=git_install.exe
 certutil -urlcache -split -f %url% %file%
@@ -32,16 +31,12 @@ certutil -urlcache -split -f %url% %file%
 
 set file="%~dp0git.exe"
 
-
 if [%file%]==[] (
-    @echo on
-    @echo Error finding "git*.exe" install executable. File may not exist or is not named with the "git" prefix.
+    echo Error finding "git*.exe" install executable. File may not exist or is not named with the "git" prefix.
     exit /b 2
 )
 
-@echo on
-@echo Installing..
-@echo off
+echo Installing git.
 %file% /VERYSILENT /LOADINF="config.inf"
 if errorlevel 1 (
     @echo on
@@ -63,16 +58,42 @@ if %errorLevel% == 0 (
 
 dotnet
 if errorlevel 0 goto installGlimmr
+echo Installing dotnet binaries.
 powershell %~dp0dotnet-install.ps1 -Channel 3.1
 
 :installGlimmr
-echo installing to "%~dp0glimmr.exe"
-git clone https://github.com/d8ahazard/glimmr ./src/
-cd ./src/
-copy ./build/win/bass.dll ../x64/bass.dll
-./publishWIN.bat
+set file="%~dp0src"
+
+if not exist %file% (
+    echo Cloning repository    
+    git clone https://github.com/d8ahazard/glimmr %~dp0src/
+) else (
+	cd %~dp0src/
+	echo Dir already exists %file%
+    git stash && git fetch && git pull
+)
+
+cd %~dp0src/
+net stop GlimmrTV
+echo Publishing for windows
+set version=1.1.0
+dotnet build HueDream.csproj /p:DeployOnBuild=true /p:PublishProfile=Windows
+dotnet build HueDream.csproj /p:DeployOnBuild=true /p:PublishProfile=Windows -o %~dp0
+echo copying bass.dll from %~dp0src\build\win\bass.dll to %~dp0x64\bass.dll
+copy %~dp0src\build\win\bass.dll %~dp0x64\bass.dll
+mkdir %~dp0wwwroot
+xcopy %~dp0src\wwwroot\* %~dp0wwwroot\ /s /i /y
+net start GlimmrTV
+pause
+if errorlevel 0 goto installService
+goto noInstall
+:installService
+echo Installing glimmr service...
 nssm install glimmr "%~dp0glimmr.exe"
 nssm set glimmr AppDirectory %~dp0
 nssm set glimmr DisplayName GlimmrTV
 nssm set glimmr Description Glimmr TV Ambient Lighting Service
 nssm set glimmr Start SERVICE_AUTO_START
+goto EOF
+:noInstall
+echo Service installation skipped.
