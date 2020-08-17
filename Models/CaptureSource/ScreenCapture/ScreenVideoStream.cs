2d82db9ad09d41e025f12e8265e7b5b39a8e4dcf
d8ahazard <d8ahazard@gmail.com>
2020-08-17 10:42:10 -0500

    Screen capture that actually works?
    Could it really have been that easy? I mean, I should probably test it before committing...but where's the fun in that.

--- Models/CaptureSource/ScreenCapture/ScreenVideoStream.cs
+++ Models/CaptureSource/ScreenCapture/ScreenVideoStream.cs
@@ -1,13 +1,10 @@
 ﻿using System;
 using System.Drawing;
-using System.Drawing.Drawing2D;
-using System.Drawing.Imaging;
 using System.Threading;
 using System.Threading.Tasks;
 using Emgu.CV;
 using Emgu.CV.Structure;
 using HueDream.Models.Util;
-using Newtonsoft.Json;
 
 namespace HueDream.Models.CaptureSource.ScreenCapture {
     public class ScreenVideoStream : IVideoStream {
@@ -32,35 +29,10 @@ namespace HueDream.Models.CaptureSource.ScreenCapture {
             var width = s.Width;
             var height = s.Height;
             Bitmap bmpScreenCapture = new Bitmap(width, height);
-            using Graphics g = Graphics.FromImage(bmpScreenCapture);
-            g.CopyFromScreen(0, 0,0, 0, bmpScreenCapture.Size, CopyPixelOperation.SourceCopy);
-            //var foo = bmpScreenCapture.ToImage(Bgr, Byte);
-            //var foo2 = bmpScreenCapture.ToMat();
-            //Frame = imageCV.Mat;
-        }
-
-
-        private Mat GetMatFromSdImage(Image image) {
-            int stride;
-            var bmp = new Bitmap(image);
-
-            var rect = new Rectangle(0, 0, bmp.Width, bmp.Height);
-            var bmpData = bmp.LockBits(rect, ImageLockMode.ReadWrite, bmp.PixelFormat);
-
-            var pf = bmp.PixelFormat;
-            if (pf == PixelFormat.Format32bppArgb) {
-                stride = bmp.Width * 4;
-            } else {
-                stride = bmp.Width * 3;
-            }
-
-            var cvImage = new Image<Bgra, byte>(bmp.Width, bmp.Height, stride, bmpData.Scan0);
-
-            bmp.UnlockBits(bmpData);
-            var output = cvImage.Mat;
-            cvImage.Dispose();
-            bmp.Dispose();
-            return output;
+            Graphics g = Graphics.FromImage(bmpScreenCapture);
+            g.CopyFromScreen(0, 0, 0, 0, s, CopyPixelOperation.SourceCopy);
+            Frame = bmpScreenCapture.ToImage<Bgr, Byte>().Mat;
+            bmpScreenCapture.Dispose();
         }
     }
 }
