8e5599dd2aa8c8b01e2e134ac58a0876047d5e01
d8ahazard <d8ahazard@gmail.com>
2020-08-20 17:14:44 -0500

    We don't need THOSE anymore...

--- publish_pi.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-REM plink -no-antispoof -pw raspberry root@192.168.1.38 service glimmr stop
-set version=1.1.0
-dotnet build HueDream.csproj /p:DeployOnBuild=true /p:PublishProfile=LinuxARM
-cd .\bin\publish\huedream-linux-arm
-echo Copying new files...
-pscp -r -pw raspberry .\* pi@192.168.1.38:/home/pi/glimmr/
-REM plink -no-antispoof -pw raspberry root@192.168.1.38 service glimmr start
-
--- publish_win.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-set version=1.1.0
-dotnet build HueDream.csproj /p:DeployOnBuild=true /p:PublishProfile=Windows
