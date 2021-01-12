bc36ee865c455929e4db3b3c39a457ad7452d83f
d8ahazard <d8ahazard@gmail.com>
2021-01-11 18:24:16 -0600

    Fun with update time for make prosperous glimmr

--- script/update_pi.sh
+++ script/update_pi.sh
@@ -1,6 +1,6 @@
 ﻿#!/bin/bash
 #Update glimmr
-log=$(ls -t glimmr* | head -1)
+log=$(ls -t /var/log/glimmr/glimmr* | head -1)
 cd /home/glimmrtv/glimmr || exit
 service glimmr stop
 echo "SERVICE STOPPED!" >> $log
@@ -8,11 +8,10 @@ git fetch && git pull
 # Build latest version
 echo "Building glimmr..." >> $log
 dotnet publish ./src/Glimmr.csproj /p:PublishProfile=LinuxARM -o ./bin/
-# cp -r /home/glimmrtv/glimmr/bin/Debug/net5.0/linux-arm/* /home/glimmrtv/glimmr/
 echo "DONE." >> $log
 # Copy necessary libraries
 echo "Copying libs..." >> $log
 cp -r /home/glimmrtv/glimmr/lib/arm/* /usr/lib
-chmod 777 ./*.sh
-echo "Restarting" >> $log
+chmod 777 ./script/*.sh
+echo "Restarting..." >> $log
 service glimmr start
