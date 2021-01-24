cd01604fb1e59b23d436e3ed433fda46832bd657
d8ahazard <d8ahazard@gmail.com>
2021-01-24 10:45:30 -0600

    Only auto-discover while not streaming
    Also, use Task.Delay instead of thread.sleep.

--- src/Services/DiscoveryService.cs
+++ src/Services/DiscoveryService.cs
@@ -1,6 +1,7 @@
 ﻿using System.Threading;
 using System.Threading.Tasks;
 using Glimmr.Hubs;
+using Glimmr.Models.ColorTarget;
 using Glimmr.Models.Util;
 using LifxNet;
 using Microsoft.AspNetCore.SignalR;
@@ -13,10 +14,12 @@ namespace Glimmr.Services {
 		// CONTROL SERVICE CONTROLS ALL
 		private readonly ControlService _controlService;
 		private LifxClient _lifxClient;
+		private bool _streaming;
 		public DiscoveryService(IHubContext<SocketServer> hubContext, ControlService controlService) {
 			_hubContext = hubContext;
 			_controlService = controlService;
 			_controlService.DeviceRescanEvent += TriggerRefresh;
+			_controlService.SetModeEvent += UpdateMode;
 			_lifxClient = _controlService.LifxClient;
 		}
 
@@ -27,7 +30,7 @@ namespace Glimmr.Services {
 				while (!stoppingToken.IsCancellationRequested) {
 					await Task.Delay(600000, stoppingToken);
 					Log.Debug("Auto-refreshing devices...");
-					TriggerRefresh();
+					if (!_streaming) TriggerRefresh();
 				}
 				return Task.CompletedTask;
 			}, stoppingToken);
@@ -36,6 +39,10 @@ namespace Glimmr.Services {
 		public override Task StopAsync(CancellationToken cancellationToken) {
 			Log.Debug("Discovery service stopped.");
 			return base.StopAsync(cancellationToken);
+		}
+
+		private void UpdateMode(int mode) {
+			_streaming = mode != 0;
 		}
 
 
@@ -52,7 +59,7 @@ namespace Glimmr.Services {
 			_lifxClient = _controlService.LifxClient;
 			DataUtil.RefreshDevices(_lifxClient, _controlService);
 			// Sleep for 5s for it to finish
-			Thread.Sleep(5000);
+			await Task.Delay(5000);
 			// Notify all clients to refresh data
 			await _hubContext.Clients.All.SendAsync("olo", DataUtil.GetStoreSerialized());
 		}
