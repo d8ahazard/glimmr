0ac879714c35aae15400b56ad71b50ee29086ab2
d8ahazard <d8ahazard@gmail.com>
2021-05-03 16:06:41 -0500

    Move scenes to src folder

--- script/publish_all.bat
+++ script/publish_all.bat
@@ -14,11 +14,7 @@ for %%x in (
 ) do (
 	echo Building %%x
 	dotnet publish -c Release ..\src\Glimmr.csproj /p:PublishProfile=%%x -o ..\src\bin\%%x
-	if exist "..\lib\%%x" xcopy /y ..\lib\%%x\* ..\src\bin\%%x\
-	if not exist "..\src\bin\ambientScenes" mkdir ..\src\bin\%%x\ambientScenes
-	if not exist "..\src\bin\audioScenes" mkdir ..\src\bin\%%x\audioScenes
-	xcopy /y ..\ambientScenes\* ..\src\bin\%%x\ambientScenes\
-	xcopy /y ..\audioScenes\* ..\src\bin\%%x\audioScenes\	
+	if exist "..\lib\%%x" xcopy /y ..\lib\%%x\* ..\src\bin\%%x\	
 )
 
 :Archive
