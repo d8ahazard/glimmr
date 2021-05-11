87875ed3ae4cf7a81d03a8db00c067e96502ab50
d8ahazard <d8ahazard@gmail.com>
2021-05-11 14:32:18 -0500

    Don't show LED strips if not running Linux (should be pi, but linux is better than nothing)

--- src/Models/ColorTarget/Led/LedDiscovery.cs
+++ src/Models/ColorTarget/Led/LedDiscovery.cs
@@ -1,12 +1,14 @@
-﻿using System.Threading;
+﻿using System.Runtime.InteropServices;
+using System.Threading;
 using System.Threading.Tasks;
 using Glimmr.Services;
+using Native;
 
 namespace Glimmr.Models.ColorTarget.Led {
 	public class LedDiscovery : ColorDiscovery, IColorDiscovery {
 		
 		public async Task Discover(CancellationToken ct, int timeout) {
-			
+			if (!RuntimeInformation.IsOSPlatform(OSPlatform.Linux)) return;
 			var ld0 = new LedData {Id = "0", Brightness = 255, GpioNumber = 18};
 			var ld1 = new LedData {Id = "1", Brightness = 255, GpioNumber = 19};
 			var ld2 = new LedData {Id = "2", Brightness = 255, GpioNumber = 10};
