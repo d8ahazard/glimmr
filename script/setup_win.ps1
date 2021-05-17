dedd2f437ee375dc4147d491de931f4b4f4a2f91
d8ahazard <d8ahazard@gmail.com>
2021-05-17 17:37:05 -0500

    Update setup_win.ps1

--- script/setup_win.ps1
+++ script/setup_win.ps1
@@ -107,7 +107,7 @@ If( -not (Test-Path -Path $glimmrPath) ){
     Invoke-Expression "& '$gitPath' pull";
 }
 
-If( (Test-Path -Path $glimmrPath) ){
+If( (Test-Path -Path $glimmrBinPath) ){
     Write-Host "Compiling Glimmr...";
     $projectPath = "$glimmrPath\src\Glimmr.csproj";
     $outPath = "$glimmrPath\bin";    
