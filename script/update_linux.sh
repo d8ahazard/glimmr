98f3da0f50bded227cb00609ffb215502dfe2336
RobertD502 <52541649+RobertD502@users.noreply.github.com>
2021-05-29 13:42:52 -0400

    Create update_linux.sh

---
+++ script/update_linux.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+log=$(ls -t /var/log/glimmr/glimmr* | head -1)
+
+#Stop service
+systemctl stop glimmr
+echo "SERVICE STOPPED!" >> $log
+
+# Fetch changes from github repo
+cd /home/glimmrtv/glimmr || exit
+git fetch && git pull
+
+# Build latest version
+echo "Building glimmr..." >> $log
+/opt/dotnet/dotnet publish /home/glimmrtv/glimmr/src/Glimmr.csproj /p:PublishProfile=Linux -o /home/glimmrtv/glimmr/bin/
+echo "DONE." >> $log
+
+#Give all scripts full permission
+chmod 777 ./script/*.sh
+echo "Restarting..." >> $log
+
+# Restart Service
+systemctl start glimmr
