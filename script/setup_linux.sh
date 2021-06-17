571ad058e2c6e94ca9bba5fa5f10dda861d9ad68
RobertD502 <52541649+RobertD502@users.noreply.github.com>
2021-06-17 17:05:48 -0400

    Yet another test, Dwight.

--- script/setup_linux.sh
+++ script/setup_linux.sh
@@ -125,16 +125,16 @@ echo "DONE."
 chmod -R 777 /home/glimmrtv/glimmr/bin
 
 #Get libcvextern.so
-if [ "$PUBPROFILE" == "Linux" ]
-then
-  echo "Installing libs for emgu.cv (Linux x64)"
-  sudo mkdir /home/glimmrtv/glimmr/lib/Linux/libcvextern
-  cd /home/glimmrtv/glimmr/lib/Linux/libcvextern || exit
-  sudo wget https://www.nuget.org/api/v2/package/Emgu.CV.runtime.ubuntu.20.04-x64/4.5.1.4349
-  unzip 4.5.1.4349
-  sudo cp /home/glimmrtv/glimmr/lib/Linux/libcvextern/runtimes/ubuntu.20.04-x64/native/libcvextern.so /home/glimmrtv/glimmr/lib/Linux/libcvextern.so
-  sudo rm -r /home/glimmrtv/glimmr/lib/Linux/libcvextern
-fi
+#if [ "$PUBPROFILE" == "Linux" ]
+#then
+#  echo "Installing libs for emgu.cv (Linux x64)"
+#  sudo mkdir /home/glimmrtv/glimmr/lib/Linux/libcvextern
+#  cd /home/glimmrtv/glimmr/lib/Linux/libcvextern || exit
+#  sudo wget https://www.nuget.org/api/v2/package/Emgu.CV.runtime.ubuntu.20.04-x64/4.5.1.4349
+#  unzip 4.5.1.4349
+#  sudo cp /home/glimmrtv/glimmr/lib/Linux/libcvextern/runtimes/ubuntu.20.04-x64/native/libcvextern.so /home/glimmrtv/glimmr/lib/Linux/libcvextern.so
+#  sudo rm -r /home/glimmrtv/glimmr/lib/Linux/libcvextern
+#fi
 
 # Copy necessary libraries
 echo "Copying libs..."
