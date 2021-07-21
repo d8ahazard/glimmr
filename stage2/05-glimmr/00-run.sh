82f6ed2bab2193f5702e7cce1b9cb83a44fbd620
d8ahazard <d8ahazard@gmail.com>
2021-07-21 17:30:13 -0500

    More fixes.

--- stage2/05-glimmr/00-run.sh
+++ stage2/05-glimmr/00-run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -e
-curl -sSL https://dot.net/v1/dotnet-install.sh | bash /dev/stdin --channel Current 
+curl -sSL https://dot.net/v1/dotnet-install.sh | bash /dev/stdin --channel Current --install-dir /opt/dotnet
 curl -sSL https://dot.net/v1/dotnet-install.sh | bash /dev/stdin --channel Current --install-dir ${ROOTFS_DIR}/opt/dotnet
 install -m 644 files/comitup.conf ${ROOTFS_DIR}/etc/comitup.conf
 mkdir -p ${ROOTFS_DIR}/home/glimmr
