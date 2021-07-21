5c7fed295d1907ebfff450c92d3d5488118d9762
d8ahazard <d8ahazard@gmail.com>
2021-07-21 16:04:09 -0500

    Revert "FFS"
    This reverts commit 70c132ffa5c94f143117a22a25ac970cf7dca8c9.

--- cleanupdev.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-df -a | grep pi-gen/work | awk '{print $6}' | sort -r | xargs umount
-
--- stage2/04-comitup/00-run.sh
+++ stage2/04-comitup/00-run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -e
 
+set -e
+
 touch ${ROOTFS_DIR}/boot/ssh
 
 mv ${ROOTFS_DIR}/etc/wpa_supplicant/wpa_supplicant.conf ${ROOTFS_DIR}/etc/wpa_supplicant/wpa_supplicant.conf.comitup_disable
@@ -7,11 +9,17 @@ mv ${ROOTFS_DIR}/etc/wpa_supplicant/wpa_supplicant.conf ${ROOTFS_DIR}/etc/wpa_su
 rm -f ${ROOTFS_DIR}/etc/network/interfaces
 install -m 644 files/interfaces ${ROOTFS_DIR}/etc/network/
 
-install -m 644 files/comitup.list ${ROOTFS_DIR}/etc/apt/sources.list.d/
-on_chroot apt-key add - < files/key-366150CE.pub.txt
+APT_DEB=$(curl https://davesteele.github.io/comitup/pkgs.json 2>&1 | grep apt-source_ | tail -1 | tr -d " ,\"")
+wget -P ${ROOTFS_DIR}/tmp https://davesteele.github.io/comitup/deb/${APT_DEB}
+
 on_chroot << EOF
+dpkg -i --force-all /tmp/davesteele-comitup-apt-source_*.deb
+apt-get -f install
 apt-get update
 systemctl mask dnsmasq.service
 systemctl mask systemd-resolved.service
+systemctl mask dhcpd.service
+systemctl mask dhcpcd.service
 EOF
 
+rm ${ROOTFS_DIR}/tmp/${APT_DEB}
