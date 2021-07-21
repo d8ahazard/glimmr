3df6f7a0f25000d9b5e66ef62ffcf3019863a1c5
d8ahazard <d8ahazard@gmail.com>
2021-07-21 14:36:06 -0500

    Updates!

--- stage2/05-glimmr/00-run.sh
+++ stage2/05-glimmr/00-run.sh
@@ -1,14 +1,5 @@
 #!/bin/bash -e
+curl -sSL https://dot.net/v1/dotnet-install.sh | bash /dev/stdin --channel Current 
+curl -sSL https://dot.net/v1/dotnet-install.sh | bash /dev/stdin --channel Current --install-dir ${ROOTFS_DIR}/opt/dotnet
 install -m 644 files/comitup.conf ${ROOTFS_DIR}/etc/comitup.conf
-mkdir -p ${ROOTFS_DIR}/home/glimmrtv
-cd ${ROOTFS_DIR}/home/glimmrtv
-# Check dotnet installation
-rm -rf ${ROOTFS_DIR}/home/glimmrtv/dotnet-sdk*
-wget https://dotnetcli.blob.core.windows.net/dotnet/Sdk/master/dotnet-sdk-latest-linux-arm.tar.gz
-sudo mkdir -p ${ROOTFS_DIR}/usr/share/dotnet
-sudo mkdir -p /usr/share/dotnet
-sudo tar -zxf dotnet-sdk-latest-linux-arm.tar.gz -C ${ROOTFS_DIR}/usr/share/dotnet
-sudo ln -sf /usr/share/dotnet/dotnet /bin/dotnet
-sudo ln -sf ${ROOTFS_DIR}/usr/share/dotnet/dotnet ${ROOTFS_DIR}/bin/dotnet
-rm -rf ${ROOTFS_DIR}/home/glimmrtv/dotnet-sdk*
-
+mkdir -p ${ROOTFS_DIR}/home/glimmr
--- stage2/05-glimmr/02-run.sh
+++ stage2/05-glimmr/02-run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -e
+echo "gpio=19=op,a5" >> ${ROOTFS_DIR}/boot/config.txt
 rm -rf ${ROOTFS_DIR}/home/glimmrtv/ws281x
 git clone https://github.com/jgarff/rpi_ws281x ${ROOTFS_DIR}/home/glimmrtv/ws281x
 cd ${ROOTFS_DIR}/home/glimmrtv/ws281x
@@ -7,19 +8,20 @@ scons
 gcc -shared -o ws2811.so *.o
 cp ./ws2811.so ${ROOTFS_DIR}/usr/lib/ws2811.so
 rm -rf ${ROOTFS_DIR}/home/glimmrtv/glimmr
+echo "done" > "${ROOTFS_DIR}/home/glimmrtv/firstrun"
 git clone -b dev https://github.com/d8ahazard/glimmr ${ROOTFS_DIR}/home/glimmrtv/glimmr
 # Install update script to init.d 
-sudo chmod 777 ${ROOTFS_DIR}/home/glimmrtv/glimmr/script/update_pi.sh
-sudo ln -sf ${ROOTFS_DIR}/home/glimmrtv/glimmr/script/update_pi.sh ${ROOTFS_DIR}/etc/init.d/update_glimmr.sh
+sudo chmod 777 ${ROOTFS_DIR}/home/glimmrtv/glimmr/script/update_linux.sh
 cd ${ROOTFS_DIR}/home/glimmrtv/glimmr
+dotnet restore ${ROOTFS_DIR}/home/glimmrtv/glimmr/src/Glimmr.csproj
 dotnet publish ${ROOTFS_DIR}/home/glimmrtv/glimmr/src/Glimmr.csproj /p:PublishProfile=LinuxARM -o ${ROOTFS_DIR}/home/glimmrtv/glimmr/bin/
+chmod -R 777 ${ROOTFS_DIR}/home/glimmrtv/glimmr/bin
 cp -r ${ROOTFS_DIR}/home/glimmrtv/glimmr/lib/bass.dll ${ROOTFS_DIR}/usr/lib/bass.dll
-cp -r ${ROOTFS_DIR}/home/glimmrtv/glimmr/lib/arm/* ${ROOTFS_DIR}/usr/lib
+cp -r ${ROOTFS_DIR}/home/glimmrtv/glimmr/lib/LinuxARM/* ${ROOTFS_DIR}/usr/lib
 # Install service
 echo "
 [Unit]
 Description=GlimmrTV
-After=network-online.target
 
 [Service]
 Type=simple
@@ -28,15 +30,11 @@ StandardOutput=tty
 Restart=always
 User=root
 WorkingDirectory=/home/glimmrtv/glimmr/bin
-ExecStart=/home/glimmrtv/glimmr/bin/Glimmr
-
+ExecStart=sudo /opt/dotnet/dotnet /home/glimmrtv/glimmr/bin/Glimmr.dll
+KillMode=process
 
 [Install]
 WantedBy=multi-user.target
 
 " > ${ROOTFS_DIR}/etc/systemd/system/glimmr.service
 sudo ln -sf ${ROOTFS_DIR}/etc/systemd/system/glimmr.service ${ROOTFS_DIR}/etc/systemd/system/multi-user.target.wants/glimmr.service
-
-rm -rf ${ROOTFS_DIR}/home/glimmrtv/comitup
-git clone https://github.com/davesteele/comitup ${ROOTFS_DIR}/home/glimmrtv/comitup
-cp -r ${ROOTFS_DIR}/home/glimmrtv/comitup/comitup/*.py ${ROOTFS_DIR}/usr/share/comitup/comitup/
