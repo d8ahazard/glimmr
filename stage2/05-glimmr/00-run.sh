7a483ed7805e58863be97c3b2d1275b4f72a77f2
d8ahazard <d8ahazard@gmail.com>
2021-07-30 09:38:33 -0500

    Update 00-run.sh

--- stage2/05-glimmr/00-run.sh
+++ stage2/05-glimmr/00-run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 curl -sSL https://dot.net/v1/dotnet-install.sh | bash /dev/stdin --channel Current --install-dir /opt/dotnet
-curl -sSL https://dot.net/v1/dotnet-install.sh | bash /dev/stdin --channel Current --install-dir ${ROOTFS_DIR}/opt/dotnet
+curl -sSL https://dot.net/v1/dotnet-install.sh | bash /dev/stdin --channel Current --architecture arm --install-dir ${ROOTFS_DIR}/opt/dotnet
 install -m 644 files/comitup.conf ${ROOTFS_DIR}/etc/comitup.conf
 install -m 644 files/ws2811.so ${ROOTFS_DIR}/usr/lib/ws2811.so
 mkdir -p ${ROOTFS_DIR}/home/glimmr
