5c7f3e3ab6e3ef1c4c34675d607d3be1435c4989
d8ahazard <d8ahazard@gmail.com>
2021-08-25 21:03:14 -0500

    That seems to work.

--- src/Glimmr/Models/ColorTarget/Adalight/AdalightDiscovery.cs
+++ src/Glimmr/Models/ColorTarget/Adalight/AdalightDiscovery.cs
@@ -20,41 +20,47 @@ namespace Glimmr.Models.ColorTarget.Adalight {
 		public async Task Discover(CancellationToken ct, int timeout) {
 			Log.Debug("Adalight: Discovery started.");
 			var discoTask = Task.Run(() => {
+				var devs = new Dictionary<string, KeyValuePair<int,int>>();
 				try {
-					var devs = FindDevices();
-					Log.Debug("Found" + devs.Count + " devices.");
-					foreach (var dev in devs) {
-						var count = dev.Value.Key;
-						var bri = dev.Value.Value;
-						try {
-							Log.Debug("Trying: " + dev.Key);
-							var ac = new AdalightNet.Adalight(dev.Key, 20);
-							ac.Connect();
-							if (ac.Connected) {
-								Log.Debug("Connected.");
-								var foo = ac.GetState();
-								count = foo[0];
-								bri = foo[0];
-								ac.Disconnect();
-								ac.Dispose();
-							} else {
-								Log.Debug("Not connected...");
-							}
-						} catch (Exception e) {
-							Log.Debug("Discovery exception: " + e.Message + " at " + e.StackTrace);
-						}
-
-
-						var data = new AdalightData(dev.Key, count);
-						if (bri != 0) {
-							data.Brightness = bri;
-						}
-
-						ControlService.AddDevice(data).ConfigureAwait(false);
-					}
+					devs = FindDevices();
 				} catch (Exception e) {
 					Log.Debug("Exception: " + e.Message);
 				}
+				Log.Debug("Found" + devs.Count + " devices.");
+
+				foreach (var dev in devs) {
+					var count = dev.Value.Key;
+					var bri = dev.Value.Value;
+					try {
+						Log.Debug("Trying: " + dev.Key);
+						var ac = new AdalightNet.Adalight(dev.Key, 20);
+						ac.Connect();
+						if (ac.Connected) {
+							Log.Debug("Connected.");
+							var foo = ac.GetState();
+							count = foo[0];
+							bri = foo[0];
+							ac.Disconnect();
+							ac.Dispose();
+							Log.Debug("State got, done.");
+						} else {
+							Log.Debug("Not connected...");
+						}
+					} catch (Exception e) {
+						Log.Debug("Discovery exception: " + e.Message + " at " + e.StackTrace);
+					}
+
+
+					var data = new AdalightData(dev.Key, count);
+					Log.Debug("Creating device.");
+					if (bri != 0) {
+						data.Brightness = bri;
+					}
+					
+					ControlService.AddDevice(data).ConfigureAwait(false);
+					Log.Debug("And added...");
+				}
+				
 			}, ct);
 			await discoTask;
 			Log.Debug("Adalight: Discovery complete.");
@@ -63,16 +69,12 @@ namespace Glimmr.Models.ColorTarget.Adalight {
 		{
 			Dictionary<string, KeyValuePair<int, int>> dictionary = new();
 			foreach (string portName in SerialPort.GetPortNames()){
-				var pnum = portName.Replace("/dev/ttyAMA","");
-				pnum = pnum.Replace("/dev/ttyACM","");
-				pnum = pnum.Replace("/dev/COM","");
-
 				Log.Debug("Testing port: " + portName);
 				try
 				{
 					SerialPort serialPort = new()
 					{
-						PortName = pnum,
+						PortName = portName,
 						BaudRate = 115200,
 						Parity = Parity.None,
 						DataBits = 8,
@@ -86,17 +88,16 @@ namespace Glimmr.Models.ColorTarget.Adalight {
 						Log.Debug("Reading");
 						var line = serialPort.ReadLine();
 						Log.Debug("Response line: " + line);
-						if (line.Substring(0, 3) == "Ada") {
-							Log.Debug("Line match");
-							dictionary[pnum] = new KeyValuePair<int, int>(0, 0);
+						if (line.Substring(0, 3).ToLower()== "ada") {
+							Log.Debug("Line match.");
+							dictionary[portName] = new KeyValuePair<int, int>(0, 0);
 						}
 						serialPort.Close();
 					} else {
 						Log.Debug($"Unable to open port {portName}.");
 					}
 				}
-				catch (Exception ex)
-				{
+				catch (Exception ex){
 					Log.Warning("Exception: " + ex.Message + " at " + ex.StackTrace);
 				}
 			}
