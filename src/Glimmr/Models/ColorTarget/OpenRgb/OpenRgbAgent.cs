a7494ab03cc273888cff034d2282f82b8fc3999e
d8ahazard <d8ahazard@gmail.com>
2021-09-08 14:33:01 -0500

    Fix RGB Agent Creation

--- src/Glimmr/Models/ColorTarget/OpenRgb/OpenRgbAgent.cs
+++ src/Glimmr/Models/ColorTarget/OpenRgb/OpenRgbAgent.cs
@@ -4,6 +4,7 @@ using System;
 using Glimmr.Models.Util;
 using Glimmr.Services;
 using OpenRGB.NET;
+using Serilog;
 
 #endregion
 
@@ -28,17 +29,22 @@ namespace Glimmr.Models.ColorTarget.OpenRgb {
 			var ip = sd.OpenRgbIp;
 			var port = sd.OpenRgbPort;
 			if (ip == Ip && port == _port && _client != null) {
+				Log.Debug("Nothing to do?");
 				return;
 			}
 
 			Ip = ip;
 			_port = port;
 			_client?.Dispose();
+			_client = null;
+			Log.Debug("Creating new client.");
+			_client = new OpenRGBClient(Ip, _port, "Glimmr",false);
+			Log.Debug("Created.");
 			try {
-				_client = new OpenRGBClient(Ip, _port, "Glimmr");
 				_client.Connect();
-			} catch (Exception) {
-				// ignored
+				Log.Debug("Connected");
+			} catch (Exception e) {
+				Log.Warning("Exception creating client: " + e.Message);
 			}
 		}
 	}
