a35ab54062cf671b9292797fb15484f27a381d69
d8ahazard <d8ahazard@gmail.com>
2021-10-13 15:55:33 -0500

    Update from base Pi-gen image

--- stage0/prerun.sh
+++ stage0/prerun.sh
@@ -1,9 +1,4 @@
 #!/bin/bash -e
-export LANGUAGE=en_US.UTF-8 
-export LANG=en_US.UTF-8
-export LC_ALL=en_US.UTF-8
-locale-gen en_US.UTF-8
-update-locale en_US.UTF-8
 
 if [ ! -d "${ROOTFS_DIR}" ] || [ "${USE_QCOW2}" = "1" ]; then
 	bootstrap ${RELEASE} "${ROOTFS_DIR}" http://raspbian.raspberrypi.org/raspbian/
