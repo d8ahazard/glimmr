09a82f50e4398b499feb9ec31c144afb9e2eff07
d8ahazard <d8ahazard@gmail.com>
2021-11-01 19:00:37 -0500

    Update installation method.
    Drop dotnet requirement.
    Install by grabbing latest release from github, versus doing git and building and stuff.

--- stage2/05-glimmr/00-run.sh
+++ stage2/05-glimmr/00-run.sh
@@ -1,8 +1,3 @@
 #!/bin/bash -e
-# Install normal dotnet so we can compile natively
-curl -sSL https://dot.net/v1/dotnet-install.sh | bash /dev/stdin --version 5.0.205 --install-dir /opt/dotnet
-# Install dotnet for ARM to our project
-curl -sSL https://dot.net/v1/dotnet-install.sh | bash /dev/stdin --version 5.0.205 --architecture arm --install-dir ${ROOTFS_DIR}/opt/dotnet
 install -m 644 files/comitup.conf ${ROOTFS_DIR}/etc/comitup.conf
 install -m 644 files/ws2811.so ${ROOTFS_DIR}/usr/lib/ws2811.so
-mkdir -p ${ROOTFS_DIR}/home/glimmr
--- stage2/05-glimmr/02-run.sh
+++ stage2/05-glimmr/02-run.sh
@@ -1,16 +1,16 @@
 #!/bin/bash -e
 echo "gpio=19=op,a5" >> ${ROOTFS_DIR}/boot/config.txt
-rm -rf ${ROOTFS_DIR}/home/glimmrtv/glimmr
 echo "done" > "${ROOTFS_DIR}/home/glimmrtv/firstrun"
-git clone -b dev https://github.com/d8ahazard/glimmr ${ROOTFS_DIR}/home/glimmrtv/glimmr
-# Install update script to init.d 
-chmod 777 ${ROOTFS_DIR}/home/glimmrtv/glimmr/script/update_linux.sh
-cd ${ROOTFS_DIR}/home/glimmrtv/glimmr
-/opt/dotnet/dotnet restore ${ROOTFS_DIR}/home/glimmrtv/glimmr/src/Glimmr/Glimmr.csproj
-/opt/dotnet/dotnet publish ${ROOTFS_DIR}/home/glimmrtv/glimmr/src/Glimmr/Glimmr.csproj /p:PublishProfile=LinuxARM -o ${ROOTFS_DIR}/home/glimmrtv/glimmr/bin/
-chmod -R 777 ${ROOTFS_DIR}/home/glimmrtv/glimmr/bin
-install -m 755 ${ROOTFS_DIR}/home/glimmrtv/glimmr/lib/bass.dll ${ROOTFS_DIR}/usr/lib/bass.dll
-cp -r ${ROOTFS_DIR}/home/glimmrtv/glimmr/lib/LinuxARM/* ${ROOTFS_DIR}/usr/lib/
+mkdir ${ROOTFS_DIR}/opt/glimmr  
+
+# Download and extract latest release
+cd ${ROOTFS_DIR}/tmp || exit
+ver=$(wget "https://api.github.com/repos/d8ahazard/glimmr/releases/latest" -q -O - | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
+url="https://github.com/d8ahazard/glimmr/releases/download/$ver/Glimmr-$PUBPATH-$ver.tgz"
+wget -O ${ROOTFS_DIR}/tmp/archive.tgz $url
+tar zxvf ${ROOTFS_DIR}/archive.tgz -C ${ROOTFS_DIR}/opt/glimmr/
+chmod -R 777 ${ROOTFS_DIR}/opt/glimmr/
+rm ${ROOTFS_DIR}/tmp/archive.tgz
 # Install service
 echo "
 [Unit]
@@ -22,8 +22,8 @@ RemainAfterExit=yes
 StandardOutput=tty
 Restart=always
 User=root
-WorkingDirectory=/home/glimmrtv/glimmr/bin
-ExecStart=sudo /opt/dotnet/dotnet /home/glimmrtv/glimmr/bin/Glimmr.dll
+WorkingDirectory=/opt/glimmr
+ExecStart=sudo Glimmr
 KillMode=process
 
 [Install]
