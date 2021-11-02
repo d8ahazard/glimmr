42ca988a336991270077fc3772c5768267b0fbab
d8ahazard <d8ahazard@gmail.com>
2021-11-02 11:32:28 -0500

    Update setup_osx.sh

--- script/setup_osx.sh
+++ script/setup_osx.sh
@@ -46,44 +46,14 @@ fi
 
 # Download and extract latest release
 cd /tmp || exit
-ver=$(wget "https://api.github.com/repos/d8ahazard/glimmr/releases/latest" -q -O - | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
+ver=$(curl -s "https://api.github.com/repos/d8ahazard/glimmr/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
 echo ver is $ver
 url="https://github.com/d8ahazard/glimmr/releases/download/$ver/Glimmr-$PUBPATH-$ver.tgz"
 echo Grabbing archive from $url
-wget -O archive.tgz $url
+curl -o ./archive.tgz $url
 tar zxvf ./archive.tgz -C $APPDIR
 chmod -R 777 $APPDIR
 rm ./archive.tgz
 echo "DONE." >> $log
 
-# Check service start/install
-if systemctl --all --type service | grep -q "$serviceName";then
-  echo "Starting Glimmr service..."
-  systemctl daemon-reload
-  service glimmr start
-  echo "DONE!"
-else  
-  echo "$serviceName does NOT exist, installing."
-  echo "
-[Unit]
-Description=GlimmrTV
-
-[Service]
-Type=simple
-RemainAfterExit=yes
-StandardOutput=tty
-Restart=always
-User=root
-WorkingDirectory=$APPDIR
-ExecStart=sudo Glimmr
-KillMode=process
-
-[Install]
-WantedBy=multi-user.target
-
-" > /etc/systemd/system/glimmr.service
-  systemctl daemon-reload
-  systemctl enable glimmr.service
-  systemctl start glimmr.service
-fi
 read -n 1 -r -s -p $'Install complete, press enter to continue. You may want to reboot now.\n'
