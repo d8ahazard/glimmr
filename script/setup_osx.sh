c928a8bab482409d3c27bcd406d71ef70feff256
d8ahazard <d8ahazard@gmail.com>
2021-11-02 11:43:01 -0500

    Update setup_osx.sh

--- script/setup_osx.sh
+++ script/setup_osx.sh
@@ -1,31 +1,23 @@
 #!/bin/sh
-# Set script variables
-
-PUBPROFILE="OSX"
-PUBFILE="osx"
-APPDIR="/Applications/glimmr"
-HOMEDIR="/Users/glimmrtv"
-USER="glimmrtv"
-
-echo "Beginning Glimmr setup/update for $PUBPROFILE"
+echo "Beginning Glimmr setup for OSX."
 
 . /etc/rc.common
-dscl . create /Users/$USER
-dscl . create /Users/$USER RealName "Glimmr TV User"
-dscl . create /Users/$USER hint "It's your username."
-dscl . create /Users/$USER picture "/Application/glimmr/wwwroot/apple-icon-144x144.png"
-dscl . passwd /Users/$USER glimmrtv
-dscl . create /Users/$USER UniqueID 420
-dscl . create /Users/$USER PrimaryGroupID 20
-dscl . create /Users/$USER UserShell /bin/bash
-dscl . create /Users/$USER NFSHomeDirectory /Users/$USER
-cp -R /System/Library/User\ Template/English.lproj /Users/$USER
-chown -R $USER:staff /Users/$USER
+dscl . create /Users/glimmrtv
+dscl . create /Users/glimmrtv RealName "Glimmr TV User"
+dscl . create /Users/glimmrtv hint "It's your username."
+dscl . create /Users/glimmrtv picture "/Application/glimmr/wwwroot/apple-icon-144x144.png"
+dscl . passwd /Users/glimmrtv glimmrtv
+dscl . create /Users/glimmrtv UniqueID 420
+dscl . create /Users/glimmrtv PrimaryGroupID 20
+dscl . create /Users/glimmrtv UserShell /bin/bash
+dscl . create /Users/glimmrtv NFSHomeDirectory /Users/glimmrtv
+cp -R /System/Library/User\ Template/English.lproj /Users/glimmrtv
+chown -R glimmrtv:staff /Users/glimmrtv
 
-if [ ! -f "$HOMEDIR/firstrun" ]
+if [ ! -f "/Users/glimmrtv/firstrun" ]
 then
   echo "Starting first-run setup..."
-  touch $HOMEDIR/firstrun
+  touch /Users/glimmrtv/firstrun
 fi
 
 if [ ! -d "/Applications/glimmr" ]
@@ -36,15 +28,15 @@ if [ ! -d "/Applications/glimmr" ]
 fi
 
 # Download and extract latest release
-cd /Users/$USER || exit
+cd /Users/glimmrtv || exit
+echo "Downloading latest release."
 ver=$(curl -s "https://api.github.com/repos/d8ahazard/glimmr/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
 echo ver is $ver
-url="https://github.com/d8ahazard/glimmr/releases/download/$ver/Glimmr-$PUBPATH-$ver.tgz"
+url="https://github.com/d8ahazard/glimmr/releases/download/$ver/Glimmr-osx-$ver.tgz"
 echo Grabbing archive from $url
 curl -o ./archive.tgz $url
-tar zxvf ./archive.tgz -C $APPDIR
-chmod -R 777 $APPDIR
+tar zxvf ./archive.tgz -C /Applications/glimmr
+chmod -R 777 /Applications/glimmr
 rm ./archive.tgz
-echo "DONE." >> $log
 
 read -n 1 -r -s -p $'Install complete, press enter to continue. You may want to reboot now.\n'
