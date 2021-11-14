5e3005fb824ff410857eeb5b8047b2380f69fc7c
d8ahazard <d8ahazard@gmail.com>
2021-11-14 17:46:33 -0600

    Update publish_osx.bat

--- script/publish_osx.bat
+++ script/publish_osx.bat
@@ -2,12 +2,12 @@
 if "%1"=="-h" GOTO HELP
 IF NOT "%2"=="-s" GOTO BUILD
 echo Stopping Glimmr
-plink -no-antispoof -pw intozoom intozoom@%1 "echo intozoom | sudo -S pkill -f Glimmr"
+plink -no-antispoof -pw Digitalhigh digitalhigh@%1 "echo digitalhigh | sudo -S pkill -f Glimmr"
 
 :BUILD
 IF NOT "%2"=="-k" GOTO BUILD2
 echo Killing Glimmr task...
-plink -no-antispoof -pw intozoom intozoom@%1 "echo intozoom | sudo -S pkill -f Glimmr"
+plink -no-antispoof -pw Digitalhigh digitalhigh@%1 "echo digitalhigh | sudo -S pkill -f Glimmr"
 
 :BUILD2
 dotnet publish ..\src\Glimmr\Glimmr.csproj /p:PublishProfile=OSX -o ..\src\Glimmr\bin\OSX --self-contained=true
@@ -22,32 +22,32 @@ IF "%2"=="-w" GOTO WEB
 
 IF "%2"=="-f" GOTO FULL
 echo Copying main files...
-pscp -P 22 -r -pw intozoom .\Glimmr.Views.dll intozoom@%1:/Library/Glimmr/1.2.0/Glimmr.Views.dll
-pscp -P 22 -r -pw intozoom .\Glimmr.deps.json intozoom@%1:/Library/Glimmr/1.2.0/Glimmr.deps.json
-pscp -P 22 -r -pw intozoom .\Glimmr.Views.pdb intozoom@%1:/Library/Glimmr/1.2.0/Glimmr.Views.pdb
-pscp -P 22 -r -pw intozoom .\wwwroot\js\* intozoom@%1:/Library/Glimmr/1.2.0/wwwroot/js/
-pscp -P 22 -r -pw intozoom .\wwwroot\css\* intozoom@%1:/Library/Glimmr/1.2.0/wwwroot/css/
-pscp -P 22 -r -pw intozoom .\Glimmr.dll intozoom@%1:/Library/Glimmr/1.2.0/Glimmr.dll
-pscp -P 22 -r -pw intozoom .\Glimmr.pdb intozoom@%1:/Library/Glimmr/1.2.0/Glimmr.pdb
-pscp -P 22 -r -pw intozoom .\Glimmr.xml intozoom@%1:/Library/Glimmr/1.2.0/Glimmr.xml
-pscp -P 22 -r -pw intozoom .\Glimmr.runtimeconfig.json intozoom@%1:/Library/Glimmr/1.2.0/Glimmr.runtimeconfig.json
-pscp -P 22 -r -pw intozoom .\Glimmr intozoom@%1:/Library/Glimmr/1.2.0/Glimmr
+pscp -P 22 -r -pw digitalhigh .\Glimmr.Views.dll Digitalhigh@%1:/Library/Glimmr/1.2.0/Glimmr.Views.dll
+pscp -P 22 -r -pw digitalhigh .\Glimmr.deps.json Digitalhigh@%1:/Library/Glimmr/1.2.0/Glimmr.deps.json
+pscp -P 22 -r -pw digitalhigh .\Glimmr.Views.pdb Digitalhigh@%1:/Library/Glimmr/1.2.0/Glimmr.Views.pdb
+pscp -P 22 -r -pw digitalhigh .\wwwroot\js\* Digitalhigh@%1:/Library/Glimmr/1.2.0/wwwroot/js/
+pscp -P 22 -r -pw digitalhigh .\wwwroot\css\* Digitalhigh@%1:/Library/Glimmr/1.2.0/wwwroot/css/
+pscp -P 22 -r -pw digitalhigh .\Glimmr.dll Digitalhigh@%1:/Library/Glimmr/1.2.0/Glimmr.dll
+pscp -P 22 -r -pw digitalhigh .\Glimmr.pdb Digitalhigh@%1:/Library/Glimmr/1.2.0/Glimmr.pdb
+pscp -P 22 -r -pw digitalhigh .\Glimmr.xml Digitalhigh@%1:/Library/Glimmr/1.2.0/Glimmr.xml
+pscp -P 22 -r -pw digitalhigh .\Glimmr.runtimeconfig.json Digitalhigh@%1:/Library/Glimmr/1.2.0/Glimmr.runtimeconfig.json
+pscp -P 22 -r -pw digitalhigh .\Glimmr Digitalhigh@%1:/Library/Glimmr/1.2.0/Glimmr
 GOTO NEXT
 :JS
 echo Copying JS files...
-pscp -P 22 -r -pw intozoom .\wwwroot\js\* intozoom@%1:/Library/Glimmr/1.2.0/wwwroot/js/
+pscp -P 22 -r -pw digitalhigh .\wwwroot\js\* Digitalhigh@%1:/Library/Glimmr/1.2.0/wwwroot/js/
 GOTO END
 :CSS
 echo Copying CSS files...
-pscp -P 22 -r -pw intozoom .\wwwroot\css\* intozoom@%1:/Library/Glimmr/1.2.0/wwwroot/css/
+pscp -P 22 -r -pw digitalhigh .\wwwroot\css\* Digitalhigh@%1:/Library/Glimmr/1.2.0/wwwroot/css/
 GOTO END
 :WEB
 echo Copying Web files...
-pscp -P 22 -r -pw intozoom .\wwwroot\* intozoom@%1:/Library/Glimmr/1.2.0/wwwroot/
+pscp -P 22 -r -pw digitalhigh .\wwwroot\* Digitalhigh@%1:/Library/Glimmr/1.2.0/wwwroot/
 GOTO END
 :FULL
 echo Copying All files...
-pscp -P 22 -r -pw intozoom .\* intozoom@%1:/Library/Glimmr/1.2.0/
+pscp -P 22 -r -pw digitalhigh .\* Digitalhigh@%1:/Library/Glimmr/1.2.0/
 :NEXT
 IF NOT "%2"=="-s" GOTO END
 
