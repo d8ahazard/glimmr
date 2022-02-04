578692a291d23c7ff881ee55a110348e8cfd5791
d8ahazard <d8ahazard@gmail.com>
2022-02-04 08:45:55 -0600

    Squashed 'Glimmr-image-gen/' content from commit 7b17b5f
    git-subtree-dir: Glimmr-image-gen
    git-subtree-split: 7b17b5f6c73273c7b265fe15684172edb4559102

---
+++ stage2/04-comitup/00-run.sh
@@ -0,0 +1,24 @@
+#!/bin/bash -e
+
+set -e
+
+touch ${ROOTFS_DIR}/boot/ssh
+
+rm -f ${ROOTFS_DIR}/etc/network/interfaces
+install -m 644 files/interfaces ${ROOTFS_DIR}/etc/network/
+
+APT_DEB=$(curl https://davesteele.github.io/comitup/pkgs.json 2>&1 | grep apt-source_ | tail -1 | tr -d " ,\"")
+wget -P ${ROOTFS_DIR}/tmp https://davesteele.github.io/comitup/deb/${APT_DEB}
+
+on_chroot << EOF
+dpkg -i --force-all /tmp/davesteele-comitup-apt-source_*.deb
+apt-get -f install
+apt-get update
+systemctl mask dnsmasq.service
+systemctl mask systemd-resolved.service
+systemctl mask dhcpd.service
+systemctl mask dhcpcd.service
+systemctl mask wpa-supplicant.service
+EOF
+
+rm ${ROOTFS_DIR}/tmp/${APT_DEB}
---
+++ stage2/05-glimmr/00-run.sh
@@ -0,0 +1,2 @@
+#!/bin/bash -e
+install -m 644 files/comitup.conf ${ROOTFS_DIR}/etc/comitup.conf
