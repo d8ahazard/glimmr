167fa2d76292cd31cbe8fed4de2103c0bd4428e8
root <root@Glimmr-image-gen>
2022-02-04 10:04:09 -0500

    Update permissions, scripts

--- stage2/05-glimmr/02-run.sh
+++ stage2/05-glimmr/02-run.sh
@@ -1,15 +1,10 @@
 #!/bin/bash -e
 echo "gpio=19=op,a5" >> ${ROOTFS_DIR}/boot/config.txt
 echo "done" > "${ROOTFS_DIR}/home/glimmrtv/firstrun"
-mkdir ${ROOTFS_DIR}/usr/share/Glimmr  
+mkdir -p ${ROOTFS_DIR}/usr/share/Glimmr  
 # Download and extract latest release
-cd /tmp || exit
-ver=$(wget "https://api.github.com/repos/d8ahazard/glimmr/releases/latest" -q -O - | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
-url="https://github.com/d8ahazard/glimmr/releases/download/$ver/Glimmr-linux-arm-$ver.tgz"
-wget -O /tmp/archive.tgz $url
-tar zxvf files/archive.tgz -C ${ROOTFS_DIR}/usr/share/Glimmr/
+tar zxvf ./files/archive.tgz -C ${ROOTFS_DIR}/usr/share/Glimmr/
 chmod -R 777 ${ROOTFS_DIR}/usr/share/Glimmr/
-rm /tmp/archive.tgz
 
 # Install service
 echo "
