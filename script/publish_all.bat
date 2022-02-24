99be3d4c9111cbd780f53182e886735d2af012b7
d8ahazard <d8ahazard@gmail.com>
2022-02-24 16:47:22 -0600

    Update publish_all.bat

--- script/publish_all.bat
+++ script/publish_all.bat
@@ -49,7 +49,8 @@ echo Creating archives...
 
 %~dp07z.exe a -ttar -so -an -r .\LinuxARM\* | %~dp07z a -si Glimmr-linux-arm-%version%.tgz
 %~dp07z.exe a -ttar -so -an -r .\LinuxARM64\* | %~dp07z a -si Glimmr-linux-arm64-%version%.tgz
-xcopy /-I /Y .\Glimmr-linux-arm64-%version%.tgz ..\..\..\Glimmr-image-gen\stage2\05-glimmr\files\archive.tgz
+xcopy /-I /Y .\Glimmr-linux-arm64-%version%.tgz ..\..\..\Glimmr-image-gen-x64\stage2\05-glimmr\files\archive.tgz
+xcopy /-I /Y .\Glimmr-linux-arm-%version%.tgz ..\..\..\Glimmr-image-gen\stage2\05-glimmr\files\archive.tgz
 %~dp07z.exe a -ttar -so -an -r .\Linux\* | %~dp07z a -si Glimmr-linux-%version%.tgz
 %~dp07z.exe a -ttar -so -an -r .\OSX\* | %~dp07z a -si Glimmr-osx-%version%.tgz
 %~dp07z.exe a -tzip -r Glimmr-windows-%version%.zip .\Windows\*
